c794ed272257d18e0bae44c743e830da4929c3c2
MohamedELgazzaR1 <58640732+MohamedELgazzaR1@users.noreply.github.com>
2020-02-20 13:39:58 +0200

    Add files via upload

---
+++ Lab1-Calculator/Calc.java
@@ -0,0 +1,19 @@
+
+public class Calc implements ICalculator {
+
+	public int add(int x,int y) {
+		return x+y;
+	}
+	public float divide (int x,int y)throws RuntimeException {		
+		
+		if(y==0)
+			throw new ArithmeticException("Math Error!") ;
+		return 	 x / (float) y ;
+		
+	}
+}
+
+
+
+
+
